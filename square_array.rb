9f04ef0b46a244b2bbb12386d95d083f6a1d88d3
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-16 22:14:00 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  my_array = []
+  array.each do |n|
+    my_array << n **2 
+  end
+  return my_array
+end
+
+def square_array_collect(array)
+  my_value.collect {|n| n*2}
 end
